8d8d2389cb32336b6267b43aa2dc829603f1aabe
tamatar <>
2021-02-07 16:34:05 +0530

    rm comments

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,7 +3,6 @@ const ytdl = require("ytdl-core");
 
 router.post("/", async (req, res) => {
   let info = await ytdl.getInfo(req.body.url);
-  // console.log(info.formats)
   res.send(info.formats);
 });
 
